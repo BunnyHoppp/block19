08455dbc9ddebf5049a6dd1d7daf25a65e772a32
BunnyHoppp <e1122434@u.nus.edu>
2025-03-18 01:36:51 +0800

    Setup GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bunnyhop.github.io/', // Set base path for GitHub Pages
+  base: '/', // Use '/' for root-level deployment
 });
